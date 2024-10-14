5e8785c6d45ed789a05939780b8264a5be686fad
Jyorish <jyorish@gmail.com>
2024-10-13 23:07:23 -0600

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var handlebars = require('hbs');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -18,15 +20,20 @@ app.set('view engine', 'hbs');
 //register handlebars partial (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
+// Bring in the database
+require('./app_api/models/db')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
